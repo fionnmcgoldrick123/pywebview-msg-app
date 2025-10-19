17649459edfeb1a1ebf8d723913b12631cdceef4
fionn mcgoldrick <fionnmc123@gmail.com>
2025-10-19 13:45:22 +0100

    add index.css and index.js. add a small non-resizable. add pywebview window with basic chat styling and chat functionality.

--- app_window.py
+++ app_window.py
@@ -1,4 +1,9 @@
 import webview
 
-webview.create_window('Hello World', 'http://127.0.0.1:5500.')
+webview.create_window('PyWebViewMessenger',
+                      'http://127.0.0.1:5500',
+                      width=400,
+                      height=500,
+                      resizable=False)
+
 webview.start()
